6e014db629f21293ec843adb263f4929ea522567
Naoki Oketani <okepy.naoki@gmail.com>
2022-12-24 14:35:53 +0000

    introduce local variables for each repo

---
+++ variables.tf
@@ -0,0 +1,59 @@
+locals {
+  repos = {
+    abc-rb = {
+      description  = "My submissions",
+      homepage_url = "https://atcoder.jp/contests/",
+      topics       = []
+    },
+    ansible-osx = {
+      description  = "Configure my macOS development environment",
+      homepage_url = "",
+      topics = [
+        "ansible",
+        "macos"
+      ]
+    },
+    contributions = {
+      description  = "count up GitHub contributions by repositories",
+      homepage_url = "",
+      topics       = []
+    },
+    kubectl-wait-sts-plugin = {
+      description  = "kubectl wait-sts plugin makes it possible to wait until statefulset gets ready",
+      homepage_url = "",
+      topics = [
+        "kubectl",
+        "kubectl-plugins",
+        "kubernetes"
+      ]
+    },
+    my-github-configuration = {
+      description  = "Configure github.com/oke-py with Terraform",
+      homepage_url = "",
+      topics = [
+        "github",
+        "terraform"
+      ]
+    },
+    npm-audit-action = {
+      description  = "GitHub Action to run `npm audit`",
+      homepage_url = "",
+      topics = [
+        "github-action",
+        "npm",
+        "security",
+        "vulnerability"
+      ]
+    },
+    programming-contest-environment = {
+      description  = "",
+      homepage_url = "",
+      topics       = []
+    },
+    zenn-content = {
+      description  = "",
+      homepage_url = "https://zenn.dev/oke_py",
+      topics       = []
+    },
+  }
+}
